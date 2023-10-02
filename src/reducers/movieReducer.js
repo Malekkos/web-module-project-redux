f4608753802835e91f4806baa1d435480af150ad
Christian Anderson <tehninjawaffle@gmail.com>
2023-10-02 11:33:52 -0700

    Added a case for the movie reducer and a action creator for movieActions

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -12,6 +12,12 @@ const reducer = (state = initialState, action) => {
             return {
                 movies: state.movies.filter(item=>(action.payload !== item.id))
             }
+        case ADD_MOVIE:
+            const movie = action.payload
+            return {
+                ...state,
+                movies: {...state.movies, movie}
+            }
         default:
             return state;
     }
